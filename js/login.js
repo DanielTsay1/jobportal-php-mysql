cd46e6d72c686a88672802d34e8fedc2c15670eb
sadLemons <brianlin1108@gmail.com>
2025-06-18 15:31:04 +0800

    first commit and randomstuff

--- js/login.js
+++ js/login.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 
 
@@ -37,4 +38,45 @@ function opendashboard(){
 }
 function backbutton(){
 	window.location.href="dashboard-job.php";
+=======
+
+
+
+let container = document.getElementById('container')
+
+
+const homepage =()=>{
+	window.location.href="index.php";
+}
+
+toggle = () => {
+	container.classList.toggle('sign-in')
+	container.classList.toggle('sign-up')
+}
+
+setTimeout(() => {
+	container.classList.add('sign-in')
+}, 200)
+
+
+
+function opendashboard(){
+	var rec=document.getElementById('recruiter');
+	var job=document.getElementById('jobseeker');
+    
+    if(job.checked && rec.checked){
+		alert("choose any one option")}
+	else if(job.checked){
+		window.location.href="dashboard.html";
+	}
+	else if(rec.checked){
+		window.location.href="recruiter.html";
+	}else
+	{
+		alert("choose any one option"); 
+	}
+}
+function backbutton(){
+	window.location.href="dashboard-job.php";
+>>>>>>> Stashed changes
 }
